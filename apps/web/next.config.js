5a9904910ada6abd479e86c2f5d4e8e7a657fb20
swifttill <malik.chatgpt26436@gmail.com>
2026-09-02 14:35:19 +0500

    Update next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  serverExternalPackages: [
+    "@prisma/client",
+    ".prisma/client"
+  ],
+};
 
 module.exports = nextConfig;
